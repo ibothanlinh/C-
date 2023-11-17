9ede947176ec57d3b78d196e59abcbf5444a4ea6
nguyen huy <danggiahuy0012@gmail.com>
2023-11-17 21:55:59 +0700

    loop, func

--- bai1/helloworld.c
+++ bai1/helloworld.c
@@ -1,26 +1,52 @@
 #include <iostream>
+#include <string>
 #include <sstream>
 using namespace std;
 
-int f1;
+// void print(int s1, int s2)
+// {
+//     cout<<"xin chao\n";
+//     cout<<"rat vui gap ban\n";
+//     cout<<"s1: "<<s1<<", s2: "<<s2;
+// }
+// void swap(int &s1, int &s2)
+// {
+//     int tmp;
+//     cout<<"2. swap()-truoc: s1: "<<s1<<", s2: "<<s2<<endl;
+
+//     tmp = s1;
+//     s1 = s2;
+//     s2 = tmp;
+//     cout<<"3. swap()-sau: s1: "<<s1<<", s2: "<<s2<<endl;
+// }
+
+int tong(int a, int b, int c=0)
+{
+    return a+b+c;
+}
 
 int main()
 {
-//    string str;
-//    cout <<"nhap: ";
-// //    cin >> str;
-//     getline(cin,str);
-//    cout <<"\ntext: "<<str;
-string str = "5521";
-int kq;
-stringstream(str)>>kq;
-cout <<"kq: "<<kq+1;
+    int a = 10;
+    int b = 20;
+    int t = tong(a,b);
+    cout <<"tong: "<<t;
+    // cout<<"1. truoc khi swap: a: "<<a<<", b: "<<b<<endl;
+    // swap(a,b);
+    // cout<<"4. sau khi swap: a: "<<a<<", b: "<<b<<endl;
+    // int t;
+    // print(a,b);
+    // cout <<"t: "<<t;6
+    // int c;
+    // c = a;
+    // a = b;
+    // b = c;
+    // cout << a<<", "<<b<<endl;
 
-if(1)
-{
-    cout <<"dung";
-}
-else
-    cout <<"sai";
-   return 0;
+    // int d = 78;
+    // int e = 90;
+    // c = d;
+    // d = e;
+    // e = c;
+    // cout << d<<", "<<e;
 }
